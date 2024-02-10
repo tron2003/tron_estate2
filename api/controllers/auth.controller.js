5c7afacea73b4c94bb9794dbbf9a32f60f4627e5
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-10 18:14:12 +0530

    commit

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,5 @@
+export const signup =(req,res)=>{
+
+console.log(req.body);
+
+}
--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import userRouter from "./routes/user.route.js";
+
+import authRouter from "./routes/auth.route.js";
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("mongodb connected !!")
 }).catch((err)=>{
@@ -15,4 +17,5 @@ app.listen(3000,()=>{
     console.log("server running on port 3000!!??");
 });
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
 
