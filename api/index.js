715e05ceebacf24cdb65160e7bd1668476e7493a
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-12 22:53:03 +0530

    ceate sign in api route

--- api/index.js
+++ api/index.js
@@ -17,19 +17,17 @@ app.listen(3000,()=>{
 
     console.log("server running on port 3000!!??");
 });
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
-app.use((err,req,res,next)=>{
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "internal server error ";
-    return res.status(statusCode).json({
-
-        success :false,
-        statusCode,
-        message,
-    });
-   
-
-    
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
+    
+
