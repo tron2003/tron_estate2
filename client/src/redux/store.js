d63837e1c4f8afe25921ad3eb6390777d278db7b
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-13 16:49:23 +0530

    added redux toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 // import { buildGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware"
 import userReducer from "./user/userslice";
+import persistReducer from "redux-persist/es/persistReducer";
+// import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({user:userReducer})
+
+const persistConfig = {
+
+  key:"root",
+  storage:storage,
+  version:1,
+
+};
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+export const persistor = persistStore(store);
+
