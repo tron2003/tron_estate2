4d1148ded1edd394b77e493a43fdf2b57691f752
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-13 20:07:48 +0530

    complete page ui part 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Signin from "./pages/signin";
-import Signout from "./pages/signout";
-import About from "./pages/about";
-import Header from "./components/header";
-import PrivateRoute from "./components/privateroute";
-import Profile from "./pages/profile"; // Import the Profile component
-import Signup from "./pages/signup.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home.jsx';
+import SignIn from './pages/signin';
+import SignUp from './pages/signup';
+import About from './pages/about';
+import Profile from './pages/profile';
+import Header from './components/header';
+import PrivateRoute from './components/privateroute';
 
 export default function App() {
   return (
-    <Router>
-      <Header /> {/* Render the Header component */}
+    <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-up" element={<Signup />} />
-        {/* Protected route for profile */}
-        <PrivateRoute path="/profile" element={<Profile />} />
-        <Route path="/sign-out" element={<Signout />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function profile() {
   return (
-    <div>
-      
+    <div className="p-3 max-w-lg max-auto">
+      <h1 className="text-3xl font-semibold text-center my-74">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center"
+        />
+        <input
+          type="text"
+          placeholder="username" id = "username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="email" id = "email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="password" id = "password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 rounded-lg p-3 hover:opacity-95 disabled:opacity-80">Update</button>
+      </form>
     </div>
-  )
+  );
 }
