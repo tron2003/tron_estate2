e57d8ad61cbe3ac6ad1271208457d39dcd60f926
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-22 19:53:51 +0530

    user listing delete functionality backend to frontend

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -142,6 +142,23 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setsaveListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -232,38 +249,42 @@ export default function Profile() {
         {showListingsError ? "Error showing listings" : ""}
       </p>
 
-
-      {saveListings &&
-        saveListings.length > 0 &&
-
-<div className="flex flex-col gap-4">
-<h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
-        {saveListings.map((listing) => (
-          <div
-            key={listing._id}
-            className="border rounded-lg p-3 flex justify-between items-center gap-4"
-          >
-            <Link to={`/listing/${listing._id}`}>
-              <img
-                src={listing.imageUrls}
-                alt="listing cover"
-                className="h-16 w-16 object-contain "
-              />
-            </Link>
-            <Link
-              className="flex-1 font-semibold  hover:underline truncate text-slate-700 "
-              to={`/listing/${listing._id}`}
+      {saveListings && saveListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {saveListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
             >
-              <p>{listing.name}</p>
-            </Link>
-            <div className="flex flex-col items-center">
-              <button className="text-red-700 uppercase">Delete</button>
-              <button className="text-green-700 uppercase">Edit</button>
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain "
+                />
+              </Link>
+              <Link
+                className="flex-1 font-semibold  hover:underline truncate text-slate-700 "
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
             </div>
-          </div>
-        ))}
-</div>
-}
+          ))}
+        </div>
+      )}
     </div>
   );
 }
