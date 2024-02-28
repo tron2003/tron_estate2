a85ba6dc043b536fc3ea95138acf534aafe21fd8
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-28 14:51:45 +0530

    complete web with bug of image not showing on home page

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function about() {
   return (
-    <div>
-      
+    <div className='py-20 px-5 max-w-6xl mx-auto'> 
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Tron Estate</h1>
+      <p className='mb-4 text-slate-700'>description</p>
+      <p className='mb-4 text-slate-700'>tod djsa</p>
     </div>
   )
 }
