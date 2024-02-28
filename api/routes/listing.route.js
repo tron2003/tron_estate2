ced2aaca197bcd59eaf71b05c8bb1afbb4d4e73c
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-28 16:42:50 +0530

    again correct path address

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createListing,deleteListing,updateListing,getListing,getListings} from '../controllers/listing.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import { verifyToken } from '../utils/verifyuser.js';
 // import { getUserListings } from '../controllers/user.controller.js';
 
 const router = express.Router();
