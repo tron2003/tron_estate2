ba0c12ec2fb9e565816b89be74f8a25a33270485
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-28 17:19:16 +0530

    Done

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -54,7 +54,7 @@ export default function Home() {
                 <img
                   src={url}
                   alt="Listing"
-                  className="h-100px  object-cover"
+                  className="h-100px  w-auto object-cover"
                 />
               </SwiperSlide>
             ))}
