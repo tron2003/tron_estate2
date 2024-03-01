7b54634eb9acb9eb30115e3abbbca0a11c56463e
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-03-01 23:07:40 +0530

    about section added

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function about() {
   return (
-    <div className='py-20 px-5 max-w-6xl mx-auto'> 
-      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Tron Estate</h1>
-      <p className='mb-4 text-slate-700'>description</p>
-      <p className='mb-4 text-slate-700'>tod djsa</p>
+    <div className="py-20 px-5 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Tron Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Hello friends i am Kanish from 3rd year Computer Science Student from
+        Punjab Engineering college.
+      </p>
+      <p className="mb-4 text-slate-700 para">
+        I made this website with the vision to implement all my knowledge that i
+        learnt from past 1 years into making this website dynamic and learning
+        about tailwindcss. Also i going to learn more techs and implement more
+        in future to make real-life use websites.
+      </p>
     </div>
-  )
+  );
 }
